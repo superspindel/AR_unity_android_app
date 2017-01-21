8084cd7d8616faf8e72de113fd572a238791caa5
superspindel <viktor.fallman@hotmail.com>
2017-01-21 12:22:08 +0100

    script för slider etc

---
+++ Assets/Scripts/Profile/AchievementPrefab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementPrefab : MonoBehaviour {
+
+	public Text achText;
+
+
+	public void Setup(AchievementObject achObj)
+	{
+		achText.text = achObj.achievementText;
+	}
+
+
+
+}
---
+++ Assets/Scripts/Profile/BadgePrefab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BadgePrefab : MonoBehaviour {
+
+	public Image badgeIcon;
+	// Use this for initialization
+	public void Setup(BadgeObject badge)
+	{
+		badgeIcon.sprite = badge.iconImage;
+	}
+}
