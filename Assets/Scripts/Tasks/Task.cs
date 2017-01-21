53cbdbceb0ea0cc571c958f23d5cfda08154f9b0
David Olsson Granlund <davols@David-Olsson-Granlund.local>
2017-01-21 13:18:36 +0100

    Beginning of task classes with available tasks list scripts

---
+++ Assets/Scripts/Tasks/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+	[System.Serializable]
+	public class Task
+	{
+		public int id;
+		public string title;
+		public List<SubTask> subtasks;
+
+		public Task ()
+		{
+		}
+	}
+}
+
---
+++ Assets/Scripts/Tasks/TaskButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application{
+public class TaskButtonScript : MonoBehaviour {
+
+	public TaskButtonScript button;
+	public Text taskLabel;
+	public Text xpLabel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
+}
