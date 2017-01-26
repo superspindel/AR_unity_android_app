53ac961c84d4339f1fd6d7213b7b111e751dc03a
superspindel <viktor.fallman@hotmail.com>
2017-01-26 16:05:37 +0100

    börjat på dataContainer

---
+++ Assets/dataContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataContainer : MonoBehaviour {
+	public List<AchievementObject> achievementList;
+	public List<BadgeObject> badgeList;
+	public Profile activeProfile;
+
+	public ArrayList settingsArray;
+
+	public List<AchievementObject> getAchievementList()
+	{
+		return this.achievementList;
+	}
+
+	public List<BadgeObject> getBadgeList()
+	{
+		return this.badgeList;
+	}
+
+	public void addAchievement(AchievementObject achObj)
+	{
+		this.achievementList.Add (achObj);
+	}
+
+	public void removeAchievement(AchievementObject achObj)
+	{
+		achievementList.Remove (achObj);
+	}
+
+	public void addBadge(BadgeObject bdgObj)
+	{
+		badgeList.Remove (bdgObj);
+	}
+
+
+}
