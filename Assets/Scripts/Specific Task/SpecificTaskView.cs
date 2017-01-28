babcafcb1c5dbf905c7b89509d2a3b93f7e931e7
Emil Jilläng <Emil Jillng>
2017-01-28 16:51:38 +0100

    More code for future use, subTask status, now using objectpool to spawn subTasks

---
+++ Assets/Scripts/Specific Task/SpecificTaskView.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SpecificTaskView : MonoBehaviour {
+	[Header("Testing Variables")]
+	[Tooltip("Use for testing")]
+	public int testListSize;
+
+	[Header("UI Gameobjects")]
+	public GameObject subTaskGroup;
+
+	[Header("Private Variable Debug")]
+	[SerializeField] private List<GameObject> subTaskList;
+	[SerializeField] private SimpleObjectPool pool;
+	[SerializeField] private float regular;
+	[SerializeField] private float bonus;
+
+
+	// Use this for initialization
+	void Start () {
+		// Init List for reference and Pool for getting the prefabs.
+		subTaskList = new List<GameObject> ();
+		pool = subTaskGroup.GetComponent<SimpleObjectPool> ();
+
+		// Test
+		generateTestList();
+		refresh ();
+	}
+
+	// When opened from bottom bar
+	void enterPage(){
+		//subTaskList = getSubTasksFromTasks (task);
+
+	}
+
+	public void refresh(){
+		refreshProgress ();
+	}
+
+	// refreshes sliders
+	void refreshProgress (){
+		float r_t = 0f; // regular total
+		float r_c = 0f; // regular completed
+		float b_t = 0f; // bonus total
+		float b_c = 0f; // bonus completed
+		foreach (GameObject g in subTaskList) {
+			SubTask subTask = g.GetComponent<SubTask> ();
+			if (!subTask.isBonus) {
+				if (subTask.status == Status.Completed) {
+					r_c++;
+				}
+				r_t++;
+			} else {
+				if (subTask.status == Status.Completed) {
+					b_c++;
+				}
+				b_t++;
+			}
+
+		}
+		regular = (r_c / r_t);
+		bonus = (b_c / b_t);
+
+		GameObject.Find ("Progress Slider").GetComponent<Slider> ().value = regular;
+		GameObject.Find ("Bonus Slider").GetComponent<Slider> ().value = bonus;
+	}
+
+	void addSubTask(int id, string name, bool isBonus, bool tool, bool help, bool info, bool warning){
+		// get prefab from objectpool
+		GameObject g = pool.GetObject ();
+		// attach to subTaskGroup LayoutGroup
+		g.transform.SetParent(subTaskGroup.transform);
+		// Set parameters
+		SubTask st = g.GetComponent<SubTask>();
+		g.GetComponent<SubTask>().setText (name + "[" + id + "]");
+		g.GetComponent<SubTask>().setBonus (isBonus);
+		g.GetComponent<SubTask>().setAvalibeButtons (tool, help, info, warning);
+		g.GetComponent<SubTask> ().setStatus (Status.InProgress);
+
+		subTaskList.Add (g);
+	}
+
+
+	// Used for testing
+	void generateTestList(){
+		for (int i = 0; i < testListSize; i++) {
+			addSubTask (i, "SubTask",  r(), r(), r(), r(), r());
+		}
+	}
+
+	private bool r(){
+		return(Random.value > 0.5);
+	}
+
+}
---
+++ Assets/Scripts/Specific Task/SubTask.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public enum Status{InProgress, Completed, Aborted};
+
+public class SubTask : MonoBehaviour {
+
+	public Status status;
+	public bool hasTool, hasHelp, hasInfo, hasWarning;
+	public bool isBonus;
+
+	private GameObject buttonGroup;
+	private Text textField;
+	private Toggle progressToggle;
+	private Toggle bonusToggle;
+
+	// Use this for initialization
+	void Awake () {
+		this.buttonGroup 	= this.transform.FindChild ("ButtonGroup").gameObject;
+		this.textField 		= this.transform.FindChild ("Name").GetComponent<Text>();
+		this.progressToggle = this.transform.FindChild ("Toggle").GetComponent<Toggle>();
+		this.bonusToggle	= this.transform.FindChild ("Toggle-Bonus").GetComponent<Toggle>();
+
+		progressToggle.onValueChanged.AddListener (toggleListener);
+		bonusToggle.onValueChanged.AddListener (toggleListener);
+	}
+
+	// When first enabled
+	void Start () {
+		refresh ();
+	}
+
+	public void setBonus(bool b){
+		isBonus = b;
+		refreshBonus ();
+	}
+
+	// Sets variables and refresh button grp();
+	public void setAvalibeButtons(bool w, bool t, bool i, bool h){
+		this.hasWarning = w;
+		this.hasTool = t;
+		this.hasInfo = i;
+		this.hasHelp = h;
+
+		refreshButtonGroup ();
+	}
+
+	// Sets title text
+	public void setText(string s){
+		this.textField.text = s;
+	}
+
+	public void toggleListener(bool b){
+		if (b) {
+			setStatus(Status.Completed);
+		} else {
+			setStatus(Status.InProgress);
+		}
+	}
+
+	// Status
+	public void setStatus(Status s){
+		this.status = s;
+		refreshTextColor ();
+		GameObject.Find ("Specific Task View").transform.GetComponent<SpecificTaskView>().refresh ();
+	}
+
+
+	// Refresh UI
+	public void refresh(){
+		refreshButtonGroup ();
+		refreshTextColor ();
+	}
+
+	private void refreshButtonGroup (){
+		this.buttonGroup.transform.Find ("Tool").gameObject.SetActive (hasTool);
+		this.buttonGroup.transform.Find ("Help").gameObject.SetActive (hasHelp);
+		this.buttonGroup.transform.Find ("Info").gameObject.SetActive (hasInfo);
+		this.buttonGroup.transform.Find ("Warning").gameObject.SetActive (hasWarning);
+	}
+
+	private void refreshTextColor(){
+		if (status == Status.InProgress) {
+			this.textField.color = Color.black;
+		}
+		if (status == Status.Completed) {
+			this.textField.color = Color.green;
+		}
+		if (status == Status.Aborted) {
+			this.textField.color = Color.red;
+		}
+	}
+
+	private void refreshBonus(){
+		this.transform.FindChild ("Toggle").gameObject.SetActive(!isBonus);
+		this.transform.FindChild ("Toggle-Bonus").gameObject.SetActive(isBonus);
+	}
+}
