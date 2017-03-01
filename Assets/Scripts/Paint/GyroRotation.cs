51019f87bd897327741403925000e2b12c29ba75
Emil Jilläng <Emil Jillng>
2017-03-01 23:07:39 +0100

    Paint on screen testing

---
+++ Assets/Scripts/Paint/GyroRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+	}
+}
