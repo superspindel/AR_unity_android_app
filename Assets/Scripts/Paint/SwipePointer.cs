31d3ab88946b96322572bf07a934b2bc71128ce8
Emil Jilläng <Emil Jillng>
2017-03-02 01:05:50 +0100

    Pointer is removed when not clicking

--- Assets/Scripts/Paint/SwipePointer.cs
+++ Assets/Scripts/Paint/SwipePointer.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwipePointer : MonoBehaviour {
-
-	Plane objPlane;
+	
+	private GameObject _model;
 
 	// Use this for initialization
 	void Start () {
-
+		_model = transform.FindChild ("Arrows Green").gameObject;
+		_model.SetActive (false);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(1)))
-		{
+		if (((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) || Input.GetMouseButton (1))) {
 			RaycastHit hit; 
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-			if ( Physics.Raycast (ray,out hit,100.0f)) {
-				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+			if (Physics.Raycast (ray, out hit, 100.0f)) {
+				_model.SetActive (true);
+				Debug.Log ("You selected the " + hit.transform.name); // ensure you picked right object
 				transform.position = hit.point;
 				transform.LookAt (hit.transform);
 			}
+		} else {
+			_model.SetActive (false);
 		}
+
 	}
 }
